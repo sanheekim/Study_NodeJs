2274678f72ed74b865b473b688764d899ca385e2
sanheekim <68312957+sanheekim@users.noreply.github.com>
2022-04-19 19:03:51 +0900

    생활코딩 WEB2 - Node.js 25강

---
+++ syntax/function.js
@@ -0,0 +1,33 @@
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log('A');
+console.log('Z');
+console.log('B');
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log('F');
+console.log('C');
+console.log('P');
+console.log('J');
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log('U');
+console.log('A');
+console.log('Z');
+console.log('J');
+console.log('I');
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+
+// 이렇게 중복이 너무 많으면
+// 가독성이 떨어짐.
+// 유지보수 하기도 힘들어짐.
+// 그래서 function2.js를 만듦.
