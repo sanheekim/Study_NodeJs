237f3ac24b3c6c85536521e156f8121abeff265e
sanheekim <68312957+sanheekim@users.noreply.github.com>
2022-04-30 23:00:50 +0900

    생활코딩 WEB2 - Node.js 39강

---
+++ syntax/object.js
@@ -0,0 +1,9 @@
+var members = ['egoing', 'k8805', 'hoya'];
+console.log(members[1]); // k8805
+
+var roles = { // object는 문법이 {}(중괄호)임.
+  'programmer':'egoing',
+  'designer' : 'k8805',
+  'manager' : 'hoya'
+}
+console.log(roles.designer); //k8805
