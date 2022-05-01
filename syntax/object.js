c1128ec4ee075809faa4807f18410fb241040476
sanheekim <68312957+sanheekim@users.noreply.github.com>
2022-05-01 22:44:46 +0900

    생활코딩 WEB2 - Node.js 40강

--- syntax/object.js
+++ syntax/object.js
@@ -1,5 +1,11 @@
 var members = ['egoing', 'k8805', 'hoya'];
 console.log(members[1]); // k8805
+// 객체의 반복
+var i = 0;
+while(i < members.length){
+  console.log('array loop', members[i]);
+  i = i + 1;
+}
 
 var roles = { // object는 문법이 {}(중괄호)임.
   'programmer':'egoing',
@@ -7,3 +13,10 @@ var roles = { // object는 문법이 {}(중괄호)임.
   'manager' : 'hoya'
 }
 console.log(roles.designer); //k8805
+console.log(roles['designer']); //k8805
+
+for(var n in roles){
+  console.log('object => ', n, 'value => ', roles[n]);
+}
+
+
