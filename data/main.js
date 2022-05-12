6e056febdf3ebc7a184ff992061a9b98a628186c
sanheekim <68312957+sanheekim@users.noreply.github.com>
2022-05-12 23:34:55 +0900

    생활코딩 WEB2 - Node.js 47강

--- data/main.js
+++ data/main.js
@@ -5,6 +5,7 @@ var qs = require('querystring');
 
 var template = require('./lib/template.js');
 var path = require('path');
+var sanitizeHtml = require('sanitize-html'); // sanitize : 출력정보에 대한 보안
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -26,16 +27,19 @@ var app = http.createServer(function(request,response){
       } else {
         fs.readdir('./data', function(error, filelist){
           var filteredId = path.parse(queryData.id).base;
-          var filteredId = path.parse(queryData.id).base;
           fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
             var title = queryData.id;
+            var sanitizedTitle = sanitizeHtml(title);
+            var sanitizedDescription = sanitizeHtml(description, {
+              allowedTags:['h1']
+            });
             var list = template.list(filelist);
-            var html = template.HTML(title, list,
-              `<h2>${title}</h2>${description}`,
+            var html = template.HTML(sanitizedTitle, list,
+              `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
               ` <a href="/create">create</a>
-                <a href="/update?id=${title}">update</a>
+              <a href="/update?id=${sanitizedTitle}">update</a>
                 <form action="delete_process" method="post">
-                  <input type="hidden" name="id" value="${title}">
+                  <input type="hidden" name="id" value="${sanitizedTitle}">
                   <input type="submit" value="delete">
                 </form>`
             );
